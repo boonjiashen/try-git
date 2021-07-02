7bb71ce50c7e12c7e3a4c9705457fc74bc9fdbbf
Jia-Shen <jiashen@gmail.com>
2021-07-03 08:47:41 +0900

    Add HelloWorld
    __Testing done__
    
    `./gradlew build` succeeds, unit test passes

---
+++ src/main/java/com/boonjiashen/try_git/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.boonjiashen.try_git;
+
+public class HelloWorld {
+
+    public void run() {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/com/boonjiashen/try_git/HelloWorldTest.java
@@ -0,0 +1,11 @@
+package com.boonjiashen.try_git;
+
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest {
+
+    @Test
+    public void runShouldSucceed() {
+        new HelloWorld().run();
+    }
+}
